e400fed0e83420492ee84f53d9d19d3389b083ef
Nilesh Narkhede <46423715+Nil15394@users.noreply.github.com>
2021-05-10 22:46:29 +0530

    Initial commit

---
+++ Hateous/src/main/java/com/nil/hateousdemo/HateousDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nil.hateousdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateousDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HateousDemoApplication.class, args);
+    }
+
+}
---
+++ Hateous/src/test/java/com/nil/hateousdemo/HateousDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nil.hateousdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateousDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
